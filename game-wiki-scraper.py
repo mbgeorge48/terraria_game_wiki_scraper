fea584de5f76518b2874f5573d01cde026bf5f65
Matt George <mbgeorge48@gmail.com>
2022-08-03 23:16:15 +0100

    Rename main to overwrite the original file

--- game-wiki-scraper.py
+++ game-wiki-scraper.py
@@ -1,84 +1,130 @@
+from datetime import datetime
 from bs4 import BeautifulSoup
 import requests
 import json
 import re
 
-URL = "https://terraria.fandom.com/wiki"
-SQUAREBRACKETTRIM = re.compile(r"\[.*?\]")
+
+class GameWikiScraper:
+    def __init__(self):
+        self.square_bracket_trim = re.compile(r"\[.*?\]")
+
+        self.base_url = "https://terraria.fandom.com/wiki"
+        self.all_items = list()
+
+        self.fetchWebPage()
+        self.filterData()
+        with open('items.json', 'w') as fp:
+            json.dump(self.all_items, fp, indent=2)
+
+    def fetchWebPage(self):
+        r = requests.get(self.base_url+"/Guide:Class_setups")
+        self.soup = BeautifulSoup(''.join(r.text), features="lxml")
+
+    def filterData(self):
+        infocards = self.soup.find_all(
+            "div", attrs={"class": "infocard clearfix"})
+        for card in infocards:
+            classContainer = card.find("div", attrs={"class": "hgroup"})
+            if len(classContainer) > 1:
+                role = classContainer.findChildren()[1].text.lower().replace(
+                    "summoning", "summoner")
+                gamestage = self.gameStageConvert(
+                    classContainer.findChildren()[0].text.lower())
+                for itemContainer in card.find_all("div", attrs={"class": "box"}):
+                    self.pullItems(itemContainer, role, gamestage)
+
+            elif classContainer.find("div", attrs={"class": "main"}).text in ["Mixed (early)", "Mixed (late)"]:
+                role = "mixed"
+                gamestage = 0
+                for itemContainer in card.find_all("div", attrs={"class": "box"}):
+                    self.pullItems(itemContainer, role, gamestage)
+
+    def gameStageConvert(self, stage):
+        gameStage = {
+            "pre-bosses": 0,
+            "pre-hardmode": 1,
+            "pre-mech bosses": 2,
+            "pre-plantera": 3,
+            "pre-golem": 4,
+            "pre-lunar events": 5,
+            "endgame": 6
+        }
+        return gameStage.get(stage, 7)
+
+    def pullItems(self, itemContainer, role, gamestage):
+        if not itemContainer.find_all("li"):
+            category = self.convertCategory(itemContainer.find(
+                "div", attrs={"class": "title"}).text.lower())
+            for span in itemContainer.find("p").find_all("span", attrs={"style": "display:block;margin:0.5em 0;"}):
+                name = span.find("a").get("title")
+                url_ext = span.find("a").get("href")
+                imageTag = span.find("a").find("img")
+                if hasattr(imageTag, "data-src") and imageTag.get("data-src"):
+                    imgPath = imageTag.get("data-src")
+                else:
+                    imgPath = imageTag.get("src")
+                this_item = {
+                    "name": name.replace("(", "").replace(")", "").rstrip(),
+                    "url": self.base_url+url_ext,
+                    "imgPath": imgPath.split(".png")[0] + ".png",
+                    "role": role,
+                    "category": category,
+                    "gameStageAvailable": gamestage}
+                self.all_items.append(this_item)
+
+        for item in itemContainer.find_all("li"):
+            valid = True
+            category = self.convertCategory(itemContainer.find(
+                "div", attrs={"class": "title"}).text.lower())
+            try:
+                itemName = item.text.replace(
+                    "(", "").replace(")", "").rstrip()
+                itemName = self.square_bracket_trim.sub("", itemName)
+            except:
+                itemName = "unknown"
+                valid = False
+            try:
+                itemURLExtension = item.find("a")["href"]
+            except:
+                itemURLExtension = "unknown"
+                valid = False
+            try:
+                itemImgPath = item.find("img")["src"].split(".png")[0]+".png"
+            except:
+                itemImgPath = "unknown"
+                valid = False
+            if valid:
+                this_item = {
+                    "name": itemName,
+                    "role": role,
+                    "url": self.base_url+itemURLExtension,
+                    "imgPath": itemImgPath,
+                    "category": category,
+                    "gameStageAvailable": gamestage}
+                self.all_items.append(this_item)
+
+    def convertCategory(self, category):
+        if "buffs" in category:
+            return 'buffs'
+        else:
+            return category
 
 
-ITEMLIST = []
+if __name__ == "__main__":
+    time_format = "%H:%M:%S"
+    start_time = datetime.now().strftime(time_format)
+    print(f"Start time = {start_time}")
+    try:
+        GameWikiScraper()
+    except KeyboardInterrupt:
+        print("Goodbye")
 
-
-def getWebPage():
-    r = requests.get(URL+"/Guide:Class_setups")
-    soup = BeautifulSoup(''.join(r.text), features="lxml")
-    infocards = soup.find_all("div", attrs={"class": "infocard clearfix"})
-    for card in infocards:
-        div = card.find("div", attrs={"class": "hgroup"})
-        if len(div) > 1:
-            role = div.findChildren()[1].text.lower().replace(
-                "summoning", "summoner")
-            gamestage = gameStageConvert(div.findChildren()[0].text.lower())
-            for box in card.find_all("div", attrs={"class": "box"}):
-                pullItems(box, role, gamestage)
-
-        elif div.find("div", attrs={"class": "main"}).text in ["Mixed (early)", "Mixed (late)"]:
-            role = "mixed"
-            gamestage = "0"
-            for box in card.find_all("div", attrs={"class": "box"}):
-                pullItems(box, role, gamestage)
-
-    with open('items.json', 'w') as fp:
-        json.dump(ITEMLIST, fp, indent=2)
-
-
-def pullItems(box, role, gamestage):
-    if not box.find_all("li"):
-        obj = {"name": box.find("p").text.replace("(", "").replace(")", "").rstrip(), "role": role, "url": URL+box.find("p").find("a")["href"], "imgPath": (
-            box.find("p").find("img")["src"].split(".png")[0]+".png"), "category": box.find("div", attrs={"class": "title"}).text.lower(), "gameStageAvailable": gamestage}
-        ITEMLIST.append(obj)
-    for item in box.find_all("li"):
-        valid = True
-        category = box.find("div", attrs={"class": "title"}).text.lower()
-        try:
-            itemName = item.text.replace(
-                "(", "").replace(")", "").rstrip()
-            itemName = SQUAREBRACKETTRIM.sub("", itemName)
-
-        except:
-            itemName = "unknown"
-            valid = False
-        try:
-            itemURLExtension = item.find("a")["href"]
-        except:
-            itemURLExtension = "unknown"
-            valid = False
-        try:
-            itemImgPath = item.find("img")["src"].split(".png")[0]+".png"
-        except:
-            itemImgPath = "unknown"
-            valid = False
-        if valid:
-            obj = {"name": itemName, "role": role,
-                   "url": URL+itemURLExtension, "imgPath": itemImgPath, "category": category, "gameStageAvailable": gamestage}
-            ITEMLIST.append(obj)
-
-
-def gameStageConvert(gameStageValue):
-    gameStage = {
-        "pre-bosses": 0,
-        "pre-hardmode": 1,
-        "pre-mech bosses": 2,
-        "pre-plantera": 3,
-        "pre-golem": 4,
-        "pre-lunar events": 5,
-        "endgame": 6
-    }
-    return gameStage.get(gameStageValue, "unknown")
-
-
-getWebPage()
-# TODO
-# Get Light Pets
-# Get Pre Boss items
+    finish_time = datetime.now().strftime(time_format)
+    print(f"Finish time = {finish_time}")
+    time_delta = datetime.strptime(finish_time, time_format) - datetime.strptime(
+        start_time, time_format
+    )
+    total_seconds = time_delta.total_seconds()
+    minutes = total_seconds / 60
+    print(f"Time elapsed = {minutes} (minutes)")
