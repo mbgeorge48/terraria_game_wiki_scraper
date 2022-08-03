d5bf0510f96c8b1557b801a5d159919918a8d2cd
Matt George <mbgeorge48@gmail.com>
2022-08-03 23:25:42 +0100

    Refactor variable names

--- game-wiki-scraper.py
+++ game-wiki-scraper.py
@@ -22,26 +22,26 @@ class GameWikiScraper:
         self.soup = BeautifulSoup(''.join(r.text), features="lxml")
 
     def filterData(self):
-        infocards = self.soup.find_all(
+        info_cards = self.soup.find_all(
             "div", attrs={"class": "infocard clearfix"})
-        for card in infocards:
-            classContainer = card.find("div", attrs={"class": "hgroup"})
-            if len(classContainer) > 1:
-                role = classContainer.findChildren()[1].text.lower().replace(
+        for card in info_cards:
+            class_container = card.find("div", attrs={"class": "hgroup"})
+            if len(class_container) > 1:
+                role = class_container.findChildren()[1].text.lower().replace(
                     "summoning", "summoner")
-                gamestage = self.gameStageConvert(
-                    classContainer.findChildren()[0].text.lower())
-                for itemContainer in card.find_all("div", attrs={"class": "box"}):
-                    self.pullItems(itemContainer, role, gamestage)
+                game_stage = self.gameStageConvert(
+                    class_container.findChildren()[0].text.lower())
+                for item_container in card.find_all("div", attrs={"class": "box"}):
+                    self.pullItems(item_container, role, game_stage)
 
-            elif classContainer.find("div", attrs={"class": "main"}).text in ["Mixed (early)", "Mixed (late)"]:
+            elif class_container.find("div", attrs={"class": "main"}).text in ["Mixed (early)", "Mixed (late)"]:
                 role = "mixed"
-                gamestage = 0
-                for itemContainer in card.find_all("div", attrs={"class": "box"}):
-                    self.pullItems(itemContainer, role, gamestage)
+                game_stage = 0
+                for item_container in card.find_all("div", attrs={"class": "box"}):
+                    self.pullItems(item_container, role, game_stage)
 
     def gameStageConvert(self, stage):
-        gameStage = {
+        game_stages = {
             "pre-bosses": 0,
             "pre-hardmode": 1,
             "pre-mech bosses": 2,
@@ -50,13 +50,13 @@ class GameWikiScraper:
             "pre-lunar events": 5,
             "endgame": 6
         }
-        return gameStage.get(stage, 7)
+        return game_stages.get(stage, 7)
 
-    def pullItems(self, itemContainer, role, gamestage):
-        if not itemContainer.find_all("li"):
-            category = self.convertCategory(itemContainer.find(
+    def pullItems(self, item_container, role, game_stage):
+        if not item_container.find_all("li"):
+            category = self.convertCategory(item_container.find(
                 "div", attrs={"class": "title"}).text.lower())
-            for span in itemContainer.find("p").find_all("span", attrs={"style": "display:block;margin:0.5em 0;"}):
+            for span in item_container.find("p").find_all("span", attrs={"style": "display:block;margin:0.5em 0;"}):
                 name = span.find("a").get("title")
                 url_ext = span.find("a").get("href")
                 imageTag = span.find("a").find("img")
@@ -70,38 +70,38 @@ class GameWikiScraper:
                     "imgPath": imgPath.split(".png")[0] + ".png",
                     "role": role,
                     "category": category,
-                    "gameStageAvailable": gamestage}
+                    "gameStageAvailable": game_stage}
                 self.all_items.append(this_item)
 
-        for item in itemContainer.find_all("li"):
+        for item in item_container.find_all("li"):
             valid = True
-            category = self.convertCategory(itemContainer.find(
+            category = self.convertCategory(item_container.find(
                 "div", attrs={"class": "title"}).text.lower())
             try:
-                itemName = item.text.replace(
+                item_name = item.text.replace(
                     "(", "").replace(")", "").rstrip()
-                itemName = self.square_bracket_trim.sub("", itemName)
+                item_name = self.square_bracket_trim.sub("", item_name)
             except:
-                itemName = "unknown"
+                item_name = "unknown"
                 valid = False
             try:
-                itemURLExtension = item.find("a")["href"]
+                item_url_extension = item.find("a")["href"]
             except:
-                itemURLExtension = "unknown"
+                item_url_extension = "unknown"
                 valid = False
             try:
-                itemImgPath = item.find("img")["src"].split(".png")[0]+".png"
+                item_img_path = item.find("img")["src"].split(".png")[0]+".png"
             except:
-                itemImgPath = "unknown"
+                item_img_path = "unknown"
                 valid = False
             if valid:
                 this_item = {
-                    "name": itemName,
+                    "name": item_name,
                     "role": role,
-                    "url": self.base_url+itemURLExtension,
-                    "imgPath": itemImgPath,
+                    "url": self.base_url+item_url_extension,
+                    "imgPath": item_img_path,
                     "category": category,
-                    "gameStageAvailable": gamestage}
+                    "gameStageAvailable": game_stage}
                 self.all_items.append(this_item)
 
     def convertCategory(self, category):
